6058d66852f08e2914330f353ee03526367a5001
PRASHANT SHARMA <prashant.dec25@gmail.com>
2021-03-08 18:03:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,5 +13,11 @@ resource "aws_eip" "myip" {
 resource "aws_instance" "myec2" {
    ami = "ami-082b5a644766e0e6f"
    instance_type = "t2.micro"
+  tags = {
+    
+    Name = "tags"
+    }
+  
+  
 }
 
